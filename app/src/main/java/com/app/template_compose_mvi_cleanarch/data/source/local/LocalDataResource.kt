456e9cb99c24cabecc728b3d3babdc0a72711b4e
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2025-04-16 19:10:19 +0700

    feat: add data starter files

---
+++ app/src/main/java/com/app/template_compose_mvi_cleanarch/data/source/local/LocalDataResource.kt
@@ -0,0 +1,7 @@
+package com.app.template_compose_mvi_cleanarch.data.source.local
+
+import com.app.template_compose_mvi_cleanarch.data.source.local.room.Dao
+
+class LocalDataResource(private val dao: Dao) {
+
+}
